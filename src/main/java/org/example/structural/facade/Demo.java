bc2d1826c9f21d1708af905bc966187c84ffbe68
Yaroslav Kondrasiuk <yvkondrasiuk@gmail.com>
2024-05-12 17:44:37 +0300

    facade

---
+++ src/main/java/org/example/structural/facade/Demo.java
@@ -0,0 +1,13 @@
+package org.example.structural.facade;
+
+import org.example.structural.facade.facade.VideoConversionFacade;
+
+import java.io.File;
+
+public class Demo {
+    public static void main(String[] args) {
+        VideoConversionFacade converter = new VideoConversionFacade();
+        File mp4Video = converter.convertVideo("youtubevideo.ogg", "mp4");
+        // ...
+    }
+}
