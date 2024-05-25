8f217374d84132742956e309193212fc0b455745
Yaroslav Kondrasiuk <yvkondrasiuk@gmail.com>
2024-05-25 19:08:07 +0300

    Mediator

--- src/main/java/org/example/behavioral/mediator/Demo.java
+++ src/main/java/org/example/behavioral/mediator/Demo.java
@@ -1,4 +1,26 @@
 package org.example.behavioral.mediator;
 
+import org.example.behavioral.mediator.components.*;
+import org.example.behavioral.mediator.mediator.Editor;
+import org.example.behavioral.mediator.mediator.Mediator;
+
+import javax.swing.*;
+
+/**
+ * Demo class. Everything comes together here.
+ */
 public class Demo {
+    public static void main(String[] args) {
+        Mediator mediator = new Editor();
+
+        mediator.registerComponent(new Title());
+        mediator.registerComponent(new TextBox());
+        mediator.registerComponent(new AddButton());
+        mediator.registerComponent(new DeleteButton());
+        mediator.registerComponent(new SaveButton());
+        mediator.registerComponent(new List(new DefaultListModel()));
+        mediator.registerComponent(new Filter());
+
+        mediator.createGUI();
+    }
 }
